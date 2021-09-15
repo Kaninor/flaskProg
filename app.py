f0d35f056a93409b40dba644a561b922c7371dcb
Kaninor <dariushroouhifard@gmail.com>
2021-09-15 15:41:24 +0430

    adding a new template and some limiting for requests

--- app.py
+++ app.py
@@ -1,16 +1,30 @@
 import os
-from flask import Flask, redirect, url_for, render_template, request, flash
+from flask import Flask, redirect, url_for, render_template, request, flash, jsonify
+from flask_limiter import Limiter
 
 app = Flask(__name__)
+limiter = Limiter(app)
 
 @app.route('/')
 def home():
-    name = "Dariush"
-    return render_template("home.html", name=name)
+    return render_template("home.html")
 
 @app.route("/profile")
 def profile():
-    return render_template("profile.html")
+    user = "Dariush"
+    return render_template("profile.html", user=user)
+
+@app.route("/admins")
+@limiter.limit("5 per 30 seconds")
+def admins():
+    adminsList = ["Dariush", "Reyhane", "Shaghayegh"]
+    return render_template("admins.html", adminsList=adminsList)
+
+@app.route("/json")
+@limiter.limit("5 per 10 seconds")
+def jsonPage():
+    data = {"message": "ok", "status": 200}
+    return jsonify(data)
 
 if __name__ == "__main__":
     app.run("0.0.0.0", 8001, debug=True)
